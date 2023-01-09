563d0dcefc148ca57ca99d0dc05d8385a5613541
thanseerthansi <thanseermedammal720@gmail.com>
2023-01-09 11:14:19 +0530

    access token time resheduled

--- used_istore/used_istore/settings.py
+++ used_istore/used_istore/settings.py
@@ -144,7 +144,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=6),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     # 'REFRESH_TOKEN_LIFETIME': timedelta(minutes=1),
     'ROTATE_REFRESH_TOKENS': False,
