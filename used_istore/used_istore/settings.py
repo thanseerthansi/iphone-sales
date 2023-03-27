a200c14f81957d6b140d16cc769e8441fe21e2b0
thanseerthansi <thanseermedammal720@gmail.com>
2023-03-27 10:42:11 +0530

    check ip

--- used_istore/used_istore/settings.py
+++ used_istore/used_istore/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['64.227.170.7']
 # ALLOWED_HOSTS = ['*']
-CORS_ORIGIN_ALLOW_ALL=True
+# CORS_ORIGIN_ALLOW_ALL=True
+CORS_ORIGIN_WHITELIST = ['http://64.227.170.7']
 
 
 # Application definition
